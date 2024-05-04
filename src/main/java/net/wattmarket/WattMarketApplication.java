ca93cb5b55ca7335499788409433263f17f8fb42
TaehwanAn <conscious52th@gmail.com>
2024-05-04 22:36:41 +0900

    최초

---
+++ src/main/java/net/wattmarket/WattMarketApplication.java
@@ -0,0 +1,13 @@
+package net.wattmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattMarketApplication.class, args);
+	}
+
+}
